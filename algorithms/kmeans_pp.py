85f68045e48302b2d79a529436ab2136cb2aca52
tboulet <timothe.boulet0@gmail.com>
2023-12-19 00:52:50 +0100

    Added KMeans++

--- algorithms/kmeans_pp.py
+++ algorithms/kmeans_pp.py
@@ -17,12 +17,30 @@ class KMeansPlusPlusAlgorithm(BaseInitForKMeansAlgorithm):
         super().__init__(config, kmeans_config)
                 
     def fit(self, x_data : np.ndarray) -> Dict[int, List[int]]:
-        raise NotImplementedError("KMeansPlusPlusAlgorithm.fit() is not implemented.")
+        
+        # Initialize centroids using the kmeans++ algorithm
+        n_clusters = self.config['k']
+        n_data, n_features = x_data.shape
+        
+        centroids = [x_data[np.random.randint(n_data)]] # Pick a random point as the first centroid
+        
+        for cluster_idx in range(1, n_clusters):
+            # For each point, compute the distance to the nearest centroid
+            distances_to_centroids = np.array([np.min(np.linalg.norm(x_data - c, axis=1)) for c in centroids])  # (n_centroids,)
+            
+            # Pick the point with the largest distance to the nearest centroid as the next centroid
+            next_centroid_idx = np.argmax(distances_to_centroids)
+            next_centroid = x_data[next_centroid_idx]
+            centroids.append(next_centroid)
+            
+        centroids = np.array(centroids)
+        
         self.kmeans_algo = KMeansAlgorithm(
             n_clusters=self.config['k'],
-            initial_centroids='k-means++',
+            initial_centroids=centroids,
             random_state=np.random.randint(1000),
             **self.kmeans_config,
         )
         labels = self.kmeans_algo.fit_predict(x_data)
         return labels_to_clustering_result(labels)   
+    
